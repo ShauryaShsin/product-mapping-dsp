dc9a4828a61d9838b2af86374fe8fd4d8c24cee7
maltheberk <66491371+maltheberk@users.noreply.github.com>
2024-05-20 10:13:24 +0200

    Clean up

--- src/model_inference.py
+++ src/model_inference.py
@@ -13,7 +13,17 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.utils import image_dataset_from_directory
 from config import IMAGE_SIZE
 
-TEST_IMAGE = "src/dataset/test/kims-havsalt/rema_image.jpg"
+start = "Start"
+
+TEST_IMAGE = "src/dataset/test_2/pringles-original/ezgif-7-c2bd800053.jpg"
+
+# Define the path to the test dataset
+TEST_PATH = pathlib.Path("src/dataset/test_2/")
+
+# List all subfolders within the main folder
+TEST2_IMAGES_FOLDERS = [
+    folder for folder in os.listdir(TEST_PATH) if (TEST_PATH / folder).is_dir()
+]
 
 
 def predict_product(img_path: str) -> str:
@@ -53,4 +63,22 @@ def predict_product(img_path: str) -> str:
     return classes[np.argmax(probabilities)]
 
 
-print("Predicted product is: ", predict_product(img_path=TEST_IMAGE))
+#
+# Loop through each subfolder
+for folder in TEST2_IMAGES_FOLDERS:
+    folder_path = TEST_PATH / folder
+    # List all image files within the subfolder
+    image_files = [
+        file
+        for file in os.listdir(folder_path)
+        if file.endswith((".png", ".jpg", ".jpeg"))
+    ]
+
+    # Loop through each image file and print its path
+    for image in image_files:
+        image_path = folder_path / image
+        print("Product", folder.capitalize())
+        print(f"Path: {image_path}")
+        result = predict_product(img_path=image_path)
+        print("Final prediction: ", result)
+        print("Is correct:", result.capitalize() == folder.capitalize())
